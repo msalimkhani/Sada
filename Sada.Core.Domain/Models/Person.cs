8e6958b5db69a27668a3583fcb308404bed7e946
Mahdi salimkhani <m.salimkhani@iau.ir>
2024-06-02 14:39:44 +0330

    Add un-entitiy Models

---
+++ Sada.Core.Domain/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sada.Core.Domain.Models
+{
+    public class Person
+    {
+        public required long NationalCode { get; set; }
+        public required int Serial { get; set; }
+        public required string Name { get; set; }
+        public required string FatherName { get; set; }
+        public required ShamsiDate BirthDate { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Address { get; set; }
+        public Person(long nationalCode, int serial, string name, string fatherName, ShamsiDate birthDate, string phoneNumber, string address)
+        {
+            NationalCode = nationalCode;
+            Serial = serial;
+            Name = name;
+            FatherName = fatherName;
+            BirthDate = birthDate;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
+    }
+}
---
+++ Sada.Core.Domain/Models/ShamsiDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sada.Core.Domain.Models
+{
+    public class ShamsiDate
+    {
+        public required int Year { get; set; }
+        public required int Month { get; set; }
+        public required int Day { get; set; }
+        public string ToString(string format)
+        {
+            return string.Format(format, Year, Month, Day);
+        }
+        public ShamsiDate(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+    }
+}
